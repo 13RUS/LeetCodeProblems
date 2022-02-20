90a5dc4cf8a368e2af3b3c13b2bc94a3391d3d03
Aleksei Leshchankin <a.leshchankin.evol@gmail.com>
2022-02-20 15:25:04 +0300

    Add files via upload

--- Array/RemoveElement_27/SolutionRemoveElement.java
+++ Array/RemoveElement_27/SolutionRemoveElement.java
@@ -2,13 +2,13 @@ package com.company;
 
 public class SolutionRemoveElement {
     public int removeElement(int[] nums, int val) {
-        int k = 0, pe = nums.length-1, b=0;
+        int k = 0, pe = nums.length - 1, b = 0;
         if (pe < 0) return k;
 
-        while (k != pe+1) {
+        while (k != pe + 1) {
 
             if (nums[k] == val) {
-                while (pe > k && nums[pe] == val ) {
+                while (pe > k && nums[pe] == val) {
                     pe--;
                 }
                 if (pe == 0 || pe == k) break;
@@ -20,9 +20,25 @@ public class SolutionRemoveElement {
             } else k++;
 
         }
-        if (pe == nums.length-1){
-            nums = new int [] {};
+        if (pe == nums.length - 1) {
+            nums = new int[]{};
         }
         return k;
+
+    }
+
+    public int removeElement2(int[] nums, int val) {
+        int i = 0;
+        int n = nums.length;
+
+        while (i < n) {
+            if (nums[i] == val) {
+                nums[i] = nums[n - 1];
+                n--;
+            } else {
+                i++;
+            }
+        }
+        return n;
     }
 }
