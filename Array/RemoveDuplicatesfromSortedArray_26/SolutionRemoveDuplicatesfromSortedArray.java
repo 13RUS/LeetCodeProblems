9f89e7a60d590bf3472bee440f2e563046e9043f
Aleksei Leshchankin <a.leshchankin.evol@gmail.com>
2022-02-21 11:41:43 +0300

    Add files via upload

--- Array/RemoveDuplicatesfromSortedArray_26/SolutionRemoveDuplicatesfromSortedArray.java
+++ Array/RemoveDuplicatesfromSortedArray_26/SolutionRemoveDuplicatesfromSortedArray.java
@@ -20,4 +20,19 @@ public class SolutionRemoveDuplicatesfromSortedArray {
 
         return k+1;
     }
+
+    public int removeDuplicates2(int[] nums) {
+        if (nums.length == 0) return 0;
+        int i = 0;
+
+        for (int j = 1; j < nums.length; j++){
+            if (nums[j] != nums[i]){
+                i++;
+                nums[i] = nums[j];
+            }
+        }
+
+        return i+1;
+    }
+
 }
