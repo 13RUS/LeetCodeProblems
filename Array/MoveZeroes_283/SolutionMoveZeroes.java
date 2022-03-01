95392950a703847dc5be0caecaa3b9d8ef5c397c
Aleksei Leshchankin <a.leshchankin.evol@gmail.com>
2022-03-01 17:21:48 +0300

    Add files via upload

--- Array/MoveZeroes_283/SolutionMoveZeroes.java
+++ Array/MoveZeroes_283/SolutionMoveZeroes.java
@@ -29,4 +29,18 @@ public class SolutionMoveZeroes {
     }
 
     }
+
+    public void moveZeroes2 (int[] nums) {
+        int lastNonZeroFoundAt = 0;
+
+        for (int i=0; i < nums.length; i++){
+            if (nums[i] != 0) {
+                nums[lastNonZeroFoundAt++] = nums[i];
+            }
+        }
+
+        for (int i = lastNonZeroFoundAt; i < nums.length; i++){
+            nums[i] = 0;
+        }
+    }
 }
