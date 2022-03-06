985897cbc6fc9eedb9cf7c926d8ae65feaf396fe
Aleksei Leshchankin <a.leshchankin.evol@gmail.com>
2022-03-06 10:58:26 +0300

    Add files via upload

--- Array/FindAllNumbersDisappearedInAnArray_448/SolutionFindAllNumbersDisappearedInAnArray.java
+++ Array/FindAllNumbersDisappearedInAnArray_448/SolutionFindAllNumbersDisappearedInAnArray.java
@@ -41,4 +41,43 @@ public class SolutionFindAllNumbersDisappearedInAnArray {
         }
         return finallist;
     }
+
+    public List<Integer> findDisappearedNumbers2(int[] nums) {
+        HashMap<Integer, Boolean> hashTable = new HashMap <Integer, Boolean>();
+
+        for (int i = 0; i < nums.length; i++){
+            hashTable.put(nums[i], true);
+        }
+
+        System.out.println(hashTable);
+
+        List <Integer> result = new LinkedList<Integer>();
+        for (int i = 1; i <= nums.length; i++){
+            if (!hashTable.containsKey(i)){
+                result.add(i);
+            }
+        }
+        return result;
+    }
+
+    public List<Integer> findDisappearedNumbers3(int[] nums) {
+
+        for (int i = 0; i < nums.length; i++) {
+            int newIndex = Math.abs(nums[i]) - 1;
+
+            if (nums[newIndex] > 0 ) {
+                nums[newIndex] *= -1;
+            }
+        }
+
+        List<Integer> result = new LinkedList<Integer>();
+
+        for (int i = 1; i <= nums.length; i++) {
+            if (nums[i-1] > 0) {
+                result.add(i);
+            }
+        }
+
+        return result;
+    }
 }
