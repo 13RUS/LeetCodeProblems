3de946a7e02f277ba894c627e27ad91b71476751
Aleksei Leshchankin <a.leshchankin.evol@gmail.com>
2022-03-14 15:15:22 +0300

    Add files via upload

---
+++ BinarySearch/Sqrt_69/Main.java
@@ -0,0 +1,15 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        int [] nums = {1,1,0,1,1,1};
+      
+        SolutionSqrt solutionSqrt = new SolutionSqrt();
+        System.out.println(solutionSqrt.mySqrt(2147483647));
+       //System.out.println(solutionSqrt.mySqrt(3364));
+
+
+    }
+}
